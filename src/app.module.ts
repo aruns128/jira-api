5828dcca7a4bedf479fd5b72e1bed4e9a9a7a3d5
Arunkumar <arunkumar.s@indiumsoft.com>
2023-07-03 17:55:58 +0530

    config things are updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RegistrationModule } from './registration/registration.module';
-import databaseConfig from './database.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from 'config.schema';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(databaseConfig), RegistrationModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: configValidationSchema,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+        };
+      },
+    }),
+    RegistrationModule,
+  ],
   controllers: [],
   providers: [],
 })
